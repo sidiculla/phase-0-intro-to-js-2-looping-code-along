d6de6dc15bf55049cd29a80d178d1d0effac077b
sojin <sojinidiculla7@gmail.com>
2021-12-13 16:03:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+                                                                                                                                       
+//function writeCards(["Guadalupe", "Ollie", "Aki"] , "surprise")
+function writeCards(array , event){
+    const newArray = [];
+    for (let i=0;i<3;i++){
+        newArray[i]= (`Thank you, ${array[i]}, for the wonderful ${event} gift!`) ;
+    }
+    return newArray;
+
+}
+
+
+function countDown(n){
+    let i =0;
+    while (i<n+1){
+        console.log(n--);
+    }
+}
